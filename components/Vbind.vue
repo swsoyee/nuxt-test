bd05bfd563156a17f677f45a7eeb2a087bf73534
swsoyee <swsoyee@gmail.com>
2020-10-17 22:36:41 +0900

    add: Vbind模块应用

---
+++ components/Vbind.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="change_color">
+        <label for="r1">修改颜色</label>
+        <input type="checkbox" v-model="use" id="r1">
+        <div :class="{'class1': use}">
+            v-bind:class 指令
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'change_color',
+    data () {
+        return {
+            use: false,
+        }
+    }
+
+}
+</script>
+
+<style>
+.class1 {
+  background: #444;
+  color: #eee;
+}
+</style>
--- pages/demo.vue
+++ pages/demo.vue
@@ -2,16 +2,19 @@
   <div>
     <p>我是demo1页面</p>
     <Hello />
+    <Vbind />
   </div>
 </template>
 
 <script>
 import Hello from '../components/Hello';
+import Vbind from '../components/Vbind';
 
 export default {
   name: 'demo',
   components: {
-    Hello
+    Hello,
+    Vbind,
   }
 }
 </script>
