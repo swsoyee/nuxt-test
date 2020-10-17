ca06c9d00335bed6aa7a86477379d10466cf8dfc
swsoyee <swsoyee@gmail.com>
2020-10-17 22:16:21 +0900

    add: 子页面中引入元件

---
+++ components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hello">
+        <h1>{{ message }}</h1>
+    </div>
+</template>>
+
+<script>
+export default {
+    name: 'hello',
+    data () {
+        return {
+            message: 'Welcome!!',
+        }
+    }
+}
+</script>
---
+++ pages/demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>我是demo1页面</p>
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from '../components/Hello';
+
+export default {
+  name: 'demo',
+  components: {
+    Hello
+  }
+}
+</script>
