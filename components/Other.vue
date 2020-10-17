715645e64dafb41939975a64babf4986072e7d1f
swsoyee <swsoyee@gmail.com>
2020-10-17 22:45:45 +0900

    add: 显示和隐藏元件

---
+++ components/Other.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <label>显示文本</label>
+      <input type="checkbox" v-model='seen'>
+      <p v-if="seen">你看到我了</p>
+      <p v-show="!seen">我隐藏起来了</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            seen: true,
+        }
+    }
+}
+</script>
--- pages/demo.vue
+++ pages/demo.vue
@@ -3,18 +3,21 @@
     <p>我是demo1页面</p>
     <Hello />
     <Vbind />
+    <Other />
   </div>
 </template>
 
 <script>
 import Hello from '../components/Hello';
 import Vbind from '../components/Vbind';
+import Other from '../components/Other';
 
 export default {
   name: 'demo',
   components: {
     Hello,
     Vbind,
+    Other,
   }
 }
 </script>
